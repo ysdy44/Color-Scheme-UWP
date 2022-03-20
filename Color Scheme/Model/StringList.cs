2c69a320b02badb48eeb77e00b40148577876ed2
ysdy44 <ysdy44@outlook.com>
2022-03-20 16:13:48 +0800

    Add StringList

---
+++ Color Scheme/Model/StringList.cs
@@ -0,0 +1,18 @@
+﻿using Color_Scheme.Strings;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Color_Scheme.Model
+{
+    public class StringList : Dictionary<LangCode, string>
+    {
+        public StringList() { }
+        public StringList(IDictionary<LangCode, string> dictionary) : base(dictionary) { }
+
+        public StringList Clone() => new StringList(this.ToDictionary
+        (
+            item => item.Key,
+            item => item.Value
+        ));
+    }
+}
