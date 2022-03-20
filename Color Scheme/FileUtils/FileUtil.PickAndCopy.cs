4b7a0f1273031bd2fcf77c5bf9ce571152bf1de0
ysdy44 <ysdy44@outlook.com>
2022-03-20 16:10:21 +0800

    Add FileUtils

---
+++ Color Scheme/FileUtils/FileUtil.PickAndCopy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Color_Scheme
+{
+    public static partial class FileUtil
+    {
+
+        public async static Task<StorageFile> CopySingleImageFileAsync(StorageFile file)
+        {
+            if (file is null) return null;
+
+            StorageFile copyFile = await file.CopyAsync(ApplicationData.Current.TemporaryFolder, file.Name, NameCollisionOption.ReplaceExisting);
+            return copyFile;
+        }
+
+        public async static Task<StorageFile> PickSingleImageFileAsync(PickerLocationId location)
+        {
+            // Picker
+            FileOpenPicker openPicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = location,
+                FileTypeFilter =
+                {
+                    ".jpg",
+                    ".jpeg",
+                    ".png",
+                    ".bmp"
+                }
+            };
+
+            // File
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            return file;
+        }
+
+        public async static Task<IReadOnlyList<StorageFile>> PickMultipleImageFilesAsync(PickerLocationId location)
+        {
+            // Picker
+            FileOpenPicker openPicker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = location,
+                FileTypeFilter =
+                {
+                    ".jpg",
+                    ".jpeg",
+                    ".png",
+                    ".bmp"
+                }
+            };
+
+            // File
+            IReadOnlyList<StorageFile> files = await openPicker.PickMultipleFilesAsync();
+            return files;
+        }
+
+    }
+}
---
+++ Color Scheme/FileUtils/FileUtil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Color_Scheme
+{
+    public static partial class FileUtil
+    {
+
+
+        #region  IsExists
+
+
+        /// <summary>
+        /// To know if a file exists.
+        /// </summary>
+        /// <param name="fileName"> The file name. </param>
+        /// <returns> The exists. </returns>
+        public static async Task<bool> IsFileExistsInLocalFolder(string fileName) => await FileUtil.IsFileExists(fileName, ApplicationData.Current.LocalFolder);
+
+        /// <summary>
+        /// To know if a file exists.
+        /// </summary>
+        /// <param name="fileName"> The file name. </param>
+        /// <returns> The exists. </returns>
+        public static async Task<bool> IsFileExistsInTemporaryFolder(string fileName) => await FileUtil.IsFileExists(fileName, ApplicationData.Current.TemporaryFolder);
+
+        /// <summary>
+        /// To know if a file exists.
+        /// </summary>
+        /// <param name="fileName"> The file name. </param>
+        /// <param name="folder"> The folder. </param>
+        /// <returns> The exists. </returns>
+        public static async Task<bool> IsFileExists(string fileName, StorageFolder folder)
+        {
+            IStorageItem item = await folder.TryGetItemAsync(fileName);
+            return (item is null) == false;
+        }
+
+
+        #endregion
+
+
+    }
+}
